c61ca23246d701431387c58808d15ed6086deebe
Ayeza <ayezabashir442@gmail.com>
2023-03-19 22:24:43 +0500

    some changes

--- 11- Custom Video Player/index.js
+++ 11- Custom Video Player/index.js
@@ -1,30 +1,45 @@
 /* Get Our Elements */
 const player = document.querySelector('.player');
+
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
+
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
 /* Build out functions */
 function togglePlay() {
-    const method = video.paused ? 'play' : 'pause';
-    video[method]();
+    /* 
+    ".paused" property tells whether the media is played or not
+     const method = video.paused ? 'play' : 'pause';
+     video[method]();
+     
+     */
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+
 }
 
 function updateButton() {
-    const icon = this.paused ? '►' : '❚ ❚';
+    const icon = this.paused ? '►' : '❚❚';
     console.log(icon);
     toggle.textContent = icon;
 }
 
 function skip() {
+    console.log(this.dataset.skip);
     video.currentTime += parseFloat(this.dataset.skip);
 }
 
 function handleRangeUpdate() {
     video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
 }
 
 function handleProgress() {
@@ -33,6 +48,8 @@ function handleProgress() {
 }
 
 function scrub(e) {
+    console.log(e);
+
     const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
     video.currentTime = scrubTime;
 }
